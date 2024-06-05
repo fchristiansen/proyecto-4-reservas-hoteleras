ba30aa9aca2ac11bd12f88b15076ad4cb304d0ff
fchristiansen <fchristiansen@gmail.com>
2024-06-05 10:55:38 -0400

    estructura y funcionalidad de métodos, post, get, put, delete

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,38 +3,98 @@ const moment = require('moment');
 
 let bookings = [];
 
-// crear reserva
+// Crear reserva
+
 exports.createBooking = async (req, res) => {
 	const { hotel, tipo_habitacion, estado_reserva, num_huespedes } = req.body;
 	const newBooking = new Booking(bookings.length + 1, hotel, tipo_habitacion, estado_reserva, num_huespedes, moment());
-
 	bookings.push(newBooking);
-
 	res.json({
 		msg: 'Reserva creada con éxito',
 		data: newBooking,
 	});
 };
 
-// obtener la lista de todas las reservas
+// Obtener la lista de reservas y segun parámetro de consulta
 
 exports.getBookings = async (req, res) => {
-	const { estado_reserva } = req.query;
+	const { hotel, fecha_inicio, fecha_fin, tipo_habitacion, estado_reserva, num_huespedes } = req.query;
 
-	if (estado_reserva) {
-		const allBookingsList = bookings.filter((booking) => booking.estado_reserva === estado_reserva);
-		if (allBookingsList.length === 0) {
+	if (hotel) {
+		const bookingsFiltered = bookings.filter((booking) => booking.hotel === hotel);
+		if (bookingsFiltered.length === 0) {
+			return res.status(404).json({ msg: 'No se encontraron usuarios' });
+		}
+
+		return res.json({
+			msg: 'Hotel:',
+			data: bookingsFiltered,
+		});
+	} else if (fecha_inicio && fecha_fin) {
+		const startDate = moment(fecha_inicio);
+		const endDate = moment(fecha_fin);
+
+		const bookingsFiltered = bookings.filter((booking) => booking.fechaCreacion.isBetween(startDate, endDate) === true);
+		if (bookingsFiltered.length === 0) {
 			return res.status(404).json({ msg: 'No se encontraron reservas' });
 		}
 
 		return res.json({
-			msg: 'Lista de Reservas',
-			data: allBookingsList,
+			msg: 'Reservas:',
+			data: bookingsFiltered,
 		});
+	} else {
+		return res.json({
+			msg: 'Reservas:',
+			data: bookings,
+		});
+	}
+};
+
+// Obtener información de una reserva específica
+
+exports.getBookingsById = async (req, res) => {
+	const bookingId = parseInt(req.params.id);
+	const booking = bookings.find((booking) => booking.id === bookingId);
+
+	if (!booking) {
+		return res.status(404).json({ msg: `No se encontraron reservas con el id: ${bookingId}` });
 	}
 
 	return res.json({
-		msg: 'Lista de todas las Reservas',
-		data: bookings,
+		msg: 'Reserva obtenida.',
+		data: booking,
 	});
+};
+
+// Actualizar información de una reserva
+
+exports.updateBookingById = async (req, res) => {
+	const bookingId = parseInt(req.params.id);
+	const bookingIndex = bookings.findIndex((booking) => booking.id === bookingId);
+
+	if (bookingIndex === -1) {
+		return res.status(404).json({ msg: `No se encontraron reservas con el id: ${bookingId}` });
+	}
+	bookings[bookingIndex] = { ...bookings[bookingIndex], ...req.body };
+
+	return res.json({
+		msg: 'Reserva actualizada con éxito.',
+		data: bookings[bookingIndex],
+	});
+};
+
+// Eliminar una reserva específica:
+
+exports.deleteBookingById = async (req, res) => {
+	const bookingId = parseInt(req.params.id);
+	const bookingIndex = bookings.findIndex((booking) => booking.id === bookingId); // 1 o 2 o 0
+
+	if (bookingIndex === -1) {
+		return res.status(404).json({ msg: `No se encontraron reservas con el id: ${bookingId}` });
+	}
+
+	bookings.splice(bookingIndex, 1);
+
+	return res.json({ msg: 'Reserva eliminada con éxito.' });
 };
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.post('/reservas', bookingController.createBooking);
 router.get('/reservas', bookingController.getBookings);
-// router.get('/reservas/:id', bookingController.getBookingsById);
-// router.put('/reservas/:id', bookingController.updateBookingById);
-// router.delete('/reservas/:id', bookingController.deleteBookingById);
+router.get('/reservas/:id', bookingController.getBookingsById);
+router.put('/reservas/:id', bookingController.updateBookingById);
+router.delete('/reservas/:id', bookingController.deleteBookingById);
 
 module.exports = router;
