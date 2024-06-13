3ba61e1aaec3af35a729cb5ad75f08f6a9866543
fchristiansen <fchristiansen@gmail.com>
2024-06-13 14:11:03 -0400

    implementacion de swagger

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const bookingRoutes = require('./routes/bookingRoutes');
+const path = require('path'); // path para trabajar con rutas de archivos y directorios
+
+//  Opciones swaggerJsDoc
+const swaggerOptions = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Node API for Hotel Reservations',
+			version: '1.0.0',
+		},
+		servers: [
+			{
+				url: serverUrl,
+			},
+		],
+	},
+	/*
+		Ruta de los archivos donde se buscarán los comentarios
+		para generar la documentación
+	*/
+
+	apis: [`${path.join(__dirname, './routes/*.js')}`],
+};
+
+// Generamos la documentación de la API
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +37,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', bookingRoutes);
+
+// Servimos la interfaz de usuario de Swagger con la documentación
+// generada en la ruta raíz
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(port, () => {
 	console.log('Servidor iniciado en el puerto ' + port);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,57 @@
+// librerias
 const express = require('express');
+const router = express.Router();
+// controlador de reservas
 const bookingController = require('../controllers/controller');
 
-const router = express.Router();
+// Definimos la especificación de Swagger para la entidad Reserva
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Reserva: { ... }
+ */
 
+// Endpoint para crear una Reserva
+/**
+ * @swagger
+ * /api/reservas:
+ *  post:
+ *    ...
+ */
 router.post('/reservas', bookingController.createBooking);
+// Endpoint para obtener la lista de reservas
+/**
+ * @swagger
+ * /api/reservas:
+ *  get:
+ *    ...
+ */
 router.get('/reservas', bookingController.getBookings);
+// Endpoint para obtener información de una reserva específica
+/**
+ * @swagger
+ * /api/reservas/{id}:
+ *  get:
+ *    ...
+ */
 router.get('/reservas/:id', bookingController.getBookingsById);
+// Endpoint para actualizar una Reserva específica
+/**
+ * @swagger
+ * /api/reservas/{id}:
+ *  put:
+ *    ...
+ */
 router.put('/reservas/:id', bookingController.updateBookingById);
+
+// Endpoint para eliminar una reserva específica
+/**
+ * @swagger
+ * /api/reservas/{id}:
+ *  delete:
+ *    ...
+ */
 router.delete('/reservas/:id', bookingController.deleteBookingById);
 
 module.exports = router;
